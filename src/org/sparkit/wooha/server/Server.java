68bb770184c37c8544e1627aa94944cc76368dec
Sriraman <sriraman.moments@gmail.com>
2011-03-26 13:25:38 +0530

    First commit

---
+++ src/org/sparkit/wooha/server/Server.java
@@ -0,0 +1,15 @@
+package org.sparkit.wooha.server;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sriraman
+ * Date: 3/26/11
+ * Time: 12:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+    public static void main(String[] args){
+        Server server = new Server() ;
+
+    }
+}
